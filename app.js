c2cdbe95d749cf615d3e2d21336516374cd41fbd
Paulo Santos <paulosantos@outlook.com.au>
2023-06-22 08:25:02 +0930

    Customer's registration form and contacts table reqdy to go.

--- app.js
+++ app.js
@@ -23,11 +23,8 @@ app.use(express.static('views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Database commands
-// db.run('DROP TABLE customers;');
-// db.run('CREATE TABLE IF NOT EXISTS customers (customer_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, bill_to TEXT, abn TEXT)');
-// db.run('INSERT INTO customers (name, bill_to, abn) VALUES (?, ?, ?)', ['Text1', 'Somewhere', '123456']);
-
+//Clearing database commands
+// db.run('DROP TABLE contacts');
 
 //Routes
 //Get methods
@@ -73,15 +70,14 @@ app.post('/regcustomer', (req, res) =>{
     }
   });
 
-  //Used to find out the customer_id created for this customer so that it can be used in the contact table as a foreign key.
+  //Used to find out the customer_id created for this customer so that it can be inserted in the contacts table as a foreign key.
   db.get('SELECT customer_id FROM customers WHERE name = ?', [companyName], (err, row) => {
+    
     if (err) {
       console.error(err.message);
     }
+
     cust_id = row.customer_id;
-    console.log('Customer ID in is: ' + cust_id);
-  
-    // console.log('Customer ID out is: ' + cust_id);
 
     db.run('INSERT INTO contacts(customer_id, name, email, phone_number, mobile_number) VALUES (?, ?, ?, ?, ?);', [cust_id, contactName, email, phone, mobile], (err) => {
       if (err) {
@@ -93,51 +89,9 @@ app.post('/regcustomer', (req, res) =>{
         res.redirect('/regcustomer');
       }
     });
-  })
-
-  // db.all("PRAGMA table_info(contacts)", (err, rows) => {
-  //   if (err) {
-  //     throw err;
-  //   }
-
-  //   // Process the result set to display the schema information
-  //   console.log("Column Name\tData Type\tNullable\tDefault Value");
-  //   console.log("-----------\t---------\t--------\t-------------");
-  //   rows.forEach(row => {
-  //     console.log(`${row.name}\t\t${row.type}\t\t${row.notnull === 0 ? "YES" : "NO"}\t\t${row.dflt_value}`);
-  //   });
-  // });
-
-  db.all('SELECT * FROM contacts', (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach(row => {
-      console.log(row);
-      // console.log(row.customer_id, row.name, row.email, row.phone_number, row.mobile_number);
-    })
-  })
-
-  // db.all('SELECT * FROM customers', (err, rows) => {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //   rows.forEach(row => {
-  //     console.log(row.customer_id, row.name, row.abn, row.address, row.city, row.state, row.postcode, row.country);
-  //   })
-  // })
-
-  db.all("SELECT * FROM contacts WHERE name = 'Paulo330'", (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach(row => {
-      console.log(row.customer_id, row.name, row.email, row.phone_number, row.mobile_number);
-    })
-  })
-
-})
+  });
+});
 
 app.listen(port, () => {
     console.log(`Example app lisntening on port ${port}`);
-})
+});
